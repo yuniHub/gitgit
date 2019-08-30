107bba2038b2cdcd2252f00a9e3e895fa70dd4d6
five0563 <38690304+five0563@users.noreply.github.com>
2019-08-30 15:57:13 +0900

    yoo PersonSirvice

---
+++ gitT/src/com/git/model/service/PersonService.java
@@ -0,0 +1,14 @@
+package com.git.model.service;
+
+import com.git.model.vo.Person;
+
+public class PersonService {
+	
+	public Person selectPerson(int pNo) {
+		
+		System.out.println("гоюл");
+		return new Person();
+		
+	}
+
+}
--- gitT/src/com/git/model/vo/Person2.java
+++
@@ -1,7 +0,0 @@
-package com.git.model.vo;
-
-public class Person2 {
-	
-	String name;
-
-}
